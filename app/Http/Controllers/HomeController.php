005502684ccd1eae51e5c69f3d670974a1534392
Nicolas Magarinus Leohnardt <nmleohnardt@gmail.com>
2026-05-14 20:21:06 -0300

    Fix problema de imagem

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,18 +14,18 @@ class HomeController extends Controller
             SELECT b.cd_bebida, b.nm_bebida, b.ds_imagem, ROUND(AVG(a.id_nota), 1) AS nota, COUNT(b.cd_bebida) AS qt_avaliacao
               FROM bebida b
               JOIN avaliacao a ON a.cd_bebida = b.cd_bebida
-             GROUP BY b.cd_bebida, b.nm_bebida, b.ds_imagem
-             ORDER BY nota DESC, qt_avaliacao DESC
+             GROUP BY b.cd_bebida, b.nm_bebida
+             ORDER BY nota DESC
              LIMIT 4
         SQL;
 
         $arrAvaliacao = DB::select($sqlAvaliacao);
 
         $sqlIngrediente = <<<SQL
-            SELECT i.nm_ingrediente, count(bi.cd_ingrediente) AS qt_utilizado
+            SELECT i.nm_ingrediente, count(bi.cd_ingrediente) AS qt_utilizado, i.ds_imagem
               FROM ingrediente i
               JOIN bebida_ingrediente bi ON bi.cd_ingrediente = i.cd_ingrediente
-             GROUP BY i.nm_ingrediente
+             GROUP BY i.nm_ingrediente, i.ds_imagem
              ORDER BY qt_utilizado DESC
              LIMIT 4
 SQL;
@@ -42,8 +42,8 @@ SQL;
         $arrRecente = DB::select($sqlRecente);
 
         return view('home')
-            ->with('arrAvaliacao', $arrAvaliacao)
+            ->with('arrAvaliacao',   $arrAvaliacao)
             ->with('arrIngrediente', $arrIngrediente)
-            ->with('arrRecente', $arrRecente);
+            ->with('arrRecente',     $arrRecente);
     }
 }
